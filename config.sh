c9241b373f064d1c283f8f9a62cb4f8e53534887
zhefeigong <zhefeigong@gmail.com>
2025-05-22 23:31:22 +0800

    set env for nvidia

--- config.sh
+++ config.sh
@@ -1,5 +1,4 @@
 
-
 ### get miniforge
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh
@@ -17,6 +16,29 @@ git config user.name
 git config user.email
 git config --list
 
-### 
+### ubuntu -> image & driver install
+
+# driver check
+lspci | grep -i vga
+ls /usr/src | grep nvidia
+lsmod | grep nvidia
+
+# driver install
+ubuntu-drivers devices
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt-get install nvidia-driver-535
+sudo reboot
+
+
+# image check
+dpkg --get-selections | grep linux-image
+uname -a
+uname -r
+
+# 'Shift'
+reboot
+linux-image-5.15.0-139-generic # for nvidia
+linux-image-5.15.76-rt53 # for franka
 
 
