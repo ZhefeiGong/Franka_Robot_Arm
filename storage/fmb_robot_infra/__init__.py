9fdcc1a18cccadaf66751f36dd8107a7be8333de
zhefeigong <zhefeigong@gmail.com>
2025-06-19 00:54:01 +0800

    archive franka control repo

--- storage/fmb_robot_infra/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='Franka-FMB-v0',
-    entry_point='envs.franka_fmb_env:FrankaFMB',
-)
--- storage/fmb_robot_infra/env.py
+++
@@ -1,4 +0,0 @@
-import gym
-import robot_infra
-env=gym.make("Franka-FMB-v0")
-
